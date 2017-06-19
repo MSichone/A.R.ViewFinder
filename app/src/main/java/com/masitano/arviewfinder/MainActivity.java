94ac89a8f226ce7ec09d65d3e151e2d048189f23
MSichone <masitano.sichone@adszambia.com>
2017-06-19 13:32:55 +0100

    Added the Introduction Slider for first time app users to describe what the app is about

--- app/src/main/java/com/masitano/arviewfinder/MainActivity.java
+++ app/src/main/java/com/masitano/arviewfinder/MainActivity.java
@@ -3,6 +3,9 @@ package com.masitano.arviewfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Loosely based on the works of
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
